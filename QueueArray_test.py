889f2ddeeea88d2916d9bf2a158e6ec51fdfcbd0
Katrina Wright <77311100+KatrinaWright@users.noreply.github.com>
2023-11-02 00:25:56 -0500

    Update QueueArray_test.py
    changes s to qa for reading comprehension clarity

--- QueueArray_test.py
+++ QueueArray_test.py
@@ -1,80 +1,80 @@
 from QueueArray import QueueArray
 
 def test_is_empty():
-    s = QueueArray()
-    assert s.is_empty()
+    qa = QueueArray()
+    assert qa.is_empty()
 
 def test_enq_empty():
-    s = QueueArray()
-    s.enq(5)
-    assert not s.is_empty()
+    qa = QueueArray()
+    qa.enq(5)
+    assert not qa.is_empty()
 
 def test_enq_tail():
-    s = QueueArray()
-    s.enq(5)
-    assert s.get_tail() == 5
+    qa = QueueArray()
+    qa.enq(5)
+    assert qa.get_tail() == 5
 
 def test_enq_front():
-    s = QueueArray()
-    s.enq(5)
-    assert s.get_front() == 5
+    qa = QueueArray()
+    qa.enq(5)
+    assert qa.get_front() == 5
 
 def test_deq():
-    s = QueueArray()
-    s.enq(5)
-    assert s.deq() == 5
+    qa = QueueArray()
+    qa.enq(5)
+    assert qa.deq() == 5
 
 def test_deq_empty():
-    s = QueueArray()
-    s.enq(5)
-    s.deq()
-    assert s.is_empty()
+    qa = QueueArray()
+    qa.enq(5)
+    qa.deq()
+    assert qa.is_empty()
 
 def test_enq_many_empty():
-    s = QueueArray()
-    s.enq(15)
-    s.enq(16)
-    s.enq(17)
-    assert not s.is_empty()
+    qa = QueueArray()
+    qa.enq(15)
+    qa.enq(16)
+    qa.enq(17)
+    assert not qa.is_empty()
 
 def test_enq_many_tail():
-    s = QueueArray()
-    s.enq(15)
-    s.enq(16)
-    s.enq(17)
-    assert s.get_tail() == 17
+    qa = QueueArray()
+    qa.enq(15)
+    qa.enq(16)
+    qa.enq(17)
+    assert qa.get_tail() == 17
 
 def test_enq_many_front():
-    s = QueueArray()
-    s.enq(15)
-    s.enq(16)
-    s.enq(17)
-    assert s.get_front() == 15
+    qa = QueueArray()
+    qa.enq(15)
+    qa.enq(16)
+    qa.enq(17)
+    assert qa.get_front() == 15
 
 def test_enq_many_deq():
-    s = QueueArray()
-    s.enq(15)
-    s.enq(16)
-    s.enq(17)
-    assert s.deq() == 15
+    qa = QueueArray()
+    qa.enq(15)
+    qa.enq(16)
+    qa.enq(17)
+    assert qa.deq() == 15
 
 def test_deq_empty():
-    s = QueueArray()
-    assert s.deq() is None
+    qa = QueueArray()
+    assert qa.deq() is None
 
 def test_clear_empty():
-    s = QueueArray()
-    s.enq(5)
-    s.clear()
-    assert s.is_empty()
+    qa = QueueArray()
+    qa.enq(5)
+    qa.clear()
+    assert qa.is_empty()
 
 def test_enq_deq_many():
-    s = QueueArray()
+    qa = QueueArray()
     le = []
     la = []
     for i in range(20,201,10):
-        s.enq(i)
+        qa.enq(i)
         le.append(i)
-    while not s.is_empty():
-        la.append(s.deq())
+    while not qa.is_empty():
+        la.append(qa.deq())
     assert le == la
